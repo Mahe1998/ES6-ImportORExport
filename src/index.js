9f38e53def342114c32c4d6c22296884db69ef5d
Mahe1998 <87573259+Mahe1998@users.noreply.github.com>
2022-03-11 22:26:26 +0530

    Final commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
+//import pi from "./math.js";
+import pi, { doublePi, triplePi } from "./math.js";
 
 ReactDOM.render(
   <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
+    <li>{pi}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
   </ul>,
   document.getElementById("root")
 );
+
+/* Use "*" (Wildcard) for importing all values.
+import * as pi from "./math.js";
+console.log(pi);
+
+ReactDOM.render(
+  <ul>
+    <li>{pi.default}</li>
+    <li>{pi.doublePi()}</li>
+    <li>{pi.triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
+*/
